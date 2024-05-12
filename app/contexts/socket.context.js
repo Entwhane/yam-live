ec9e2671eb1bdb6d4fff124c7253b7a0b6344e9a
Antoine Lascou <antoine.lascou@epsi.fr>
2024-05-12 18:25:28 +0200

    sdk 51

--- app/contexts/socket.context.js
+++ app/contexts/socket.context.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Platform } from 'react-native'; import io from "socket.io-client";
 
 console.log('Emulation OS Platform: ', Platform.OS);
-export const socketEndpoint = Platform.OS === 'web' ? "http://localhost:3000" : "http://10.60.104.98:3000";
+export const socketEndpoint = Platform.OS === 'web' ? "http://localhost:3000" : "http://192.168.1.10:3000";
 
 export const socket = io(socketEndpoint, {
     transports: ["websocket"],
